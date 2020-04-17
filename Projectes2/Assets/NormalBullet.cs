a43633ca1a92b77c740a3d3aba51eb94132594b5
Marc <marcgaldo.97@gmail.com>
2020-04-17 19:25:48 +0200

    Bullets Completed without mechanics

---
+++ Projectes2/Assets/NormalBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalBullet : MonoBehaviour
+{
+    public int BulletSpeed;
+    private Vector3 forward;
+    // Start is called before the first frame update
+    void Start()
+    {
+        forward = new Vector3(-1, -1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(forward * BulletSpeed * Time.deltaTime);
+
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.CompareTag("Hitable"))
+        {
+                      Destroy(this.gameObject);
+        }
+    }
+}
