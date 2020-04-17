7fcc44945c9c09c0a67819177340d3e87229db13
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2020-04-17 23:03:09 +0200

    slime

--- Projectes2/Assets/Scripts/Enemies/Slime Enemie/SlimeEnemieProjectile.cs
+++ Projectes2/Assets/Scripts/Enemies/Slime Enemie/SlimeEnemieProjectile.cs
@@ -5,24 +5,41 @@ using UnityEngine;
 public class SlimeEnemieProjectile : MonoBehaviour
 {
     public Transform throwPoint;
-
+    public Transform playerTarget;
     public GameObject projectile;
+    public float timeTilHit = 1f;
 
     public float xVelo, yVelo;
 
     
     void Start()
     {
+        playerTarget = GameObject.Find("Player").transform;
         Trow();
     }
 
     void Trow()
     {
 
-        GameObject bulletInstace = Instantiate(projectile, throwPoint.position, Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
-        Rigidbody2D rigid;
-        rigid = bulletInstace.GetComponent<Rigidbody2D>();
+        float xdistance;
+        xdistance = playerTarget.position.x - throwPoint.position.x;
 
-        rigid.velocity = new Vector2(xVelo, yVelo);
+        float ydistance;
+        ydistance = playerTarget.position.y - throwPoint.position.y;
+
+        float trowAngle;
+
+        trowAngle = Mathf.Atan((ydistance + 4.905f) / xdistance);
+
+        float totalvelo = xdistance / Mathf.Cos(trowAngle);
+
+        xVelo = totalvelo * Mathf.Cos(trowAngle);
+        yVelo = totalvelo * Mathf.Sin(trowAngle);
+
+        GameObject bulletInstace  = Instantiate(projectile, throwPoint.position, Quaternion.Euler(new Vector3(0,0,0))) as GameObject;
+        Rigidbody2D rd;
+        rd = bulletInstace.GetComponent<Rigidbody2D>();
+
+        rd.velocity = new Vector2(xVelo, yVelo);
     }
 }
