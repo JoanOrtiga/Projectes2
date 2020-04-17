b2e1405a4c66472fc3738a1751192b824e939765
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2020-04-17 21:26:14 +0200

    Slime Enemie

---
+++ Projectes2/Assets/Scripts/Enemies/Slime Enemie/SlimeEnemieProjectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeEnemieProjectile : MonoBehaviour
+{
+    public Transform throwPoint;
+
+    public GameObject projectile;
+
+    public float xVelo, yVelo;
+
+    
+    void Start()
+    {
+        Trow();
+    }
+
+    void Trow()
+    {
+
+        GameObject bulletInstace = Instantiate(projectile, throwPoint.position, Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
+        Rigidbody2D rigid;
+        rigid = bulletInstace.GetComponent<Rigidbody2D>();
+
+        rigid.velocity = new Vector2(xVelo, yVelo);
+    }
+}
---
+++ Projectes2/Assets/Scripts/Enemies/Slime Enemie/SlimeEnemieSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeEnemieSystem : MonoBehaviour
+{
+    public int heath = 10;
+
+
+    public Transform[] spawnPoints;
+    public GameObject[] enemie;
+
+
+    private void Start()
+    {
+        if (transform.childCount > 1)
+        {
+            spawnPoints[0] = this.gameObject.transform.GetChild(0);
+            spawnPoints[1] = this.gameObject.transform.GetChild(1);
+        }
+        else
+        {
+            print("THis slime has no childern");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemie.Length == 0 && heath <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        else if (heath <= 0 && enemie.Length > 0)
+        {
+            Instantiate(enemie[0], spawnPoints[0].position, Quaternion.identity);
+            Instantiate(enemie[0], spawnPoints[1].position, Quaternion.identity);
+            Destroy(this.gameObject);
+            
+        }
+       
+    }
+}
