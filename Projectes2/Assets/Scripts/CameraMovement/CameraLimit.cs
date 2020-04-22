c52f0571912fc7fa7be853bf9f94ca495f2d4c54
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-04-22 16:49:01 +0200

    Camera

---
+++ Projectes2/Assets/Scripts/CameraMovement/CameraLimit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CameraLimit : MonoBehaviour
+{
+    public UnityEvent cameraTouchedLimit;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        cameraTouchedLimit.Invoke();
+    }
+}
