64789f532c102789e27c2dabe3a174f7e49e20c5
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-04-23 17:03:41 +0200

    FixCamera

---
+++ Projectes2/Assets/Scripts/CameraMovement/CameraLimit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CameraLimit : MonoBehaviour
+{
+    public Transform limit1;
+    public Transform limit2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Camera.main.GetComponent<CameraController>().NewRoom(limit1.position, limit2.position);
+        }
+    }
+}
