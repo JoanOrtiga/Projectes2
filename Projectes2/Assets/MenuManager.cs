ff57b92eb77adf92af7b190dcb6137c0ebaed768
Marc <marcgaldo.97@gmail.com>
2020-04-24 12:58:35 +0200

    Main Menu  exit and play

--- Projectes2/Assets/MenuManager.cs
+++ Projectes2/Assets/MenuManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MenuManager : MonoBehaviour
 {
@@ -21,6 +23,6 @@ public class MenuManager : MonoBehaviour
     }
     public void start()
     {
-
+        SceneManager.LoadScene("Level1");
     }
 }
