56c663d904b560b0f2e5ec1ca7f8bc0874131c2d
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-05-13 12:59:13 +0200

    Update

---
+++ Projectes2/Assets/Scripts/CheckPoint/CheckPointManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CheckPointManager : MonoBehaviour
+{
+    public GameObject player;
+
+    public UnityEvent RespawnEnemies;
+    Vector2 StartingPosition;
+
+    public void Restart()
+    {
+        player.transform.position = StartingPosition;
+        RespawnEnemies.Invoke();
+    }
+
+    public void GetCheckPoint(Vector2 pos)
+    {
+        StartingPosition = pos;
+    }
+}
