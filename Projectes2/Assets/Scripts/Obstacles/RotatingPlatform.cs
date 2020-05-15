8f4413d4594a22cdc297fcc74449f611a27959a1
Mr_Serra <posefa11@gmail.com>
2020-05-15 06:59:14 +0200

    All night looOOOooong

---
+++ Projectes2/Assets/Scripts/Obstacles/RotatingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPlatform : MonoBehaviour
+{
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (gameObject.GetComponent<Collider2D>().isTrigger )
+        {
+            gameObject.transform.rotation = Quaternion.EulerRotation(0, 0, 1);
+
+        }
+    }
+}
