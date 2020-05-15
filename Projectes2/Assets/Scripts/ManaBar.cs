a7989040d10237ef11a1beb00b20c6093047ae79
Marc <marcgaldo.97@gmail.com>
2020-05-15 06:57:12 +0200

    Sistema de Sonido To guapo

---
+++ Projectes2/Assets/Scripts/ManaBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+    public GameObject BulletManager;
+    private void Start()
+    {
+        GetComponent<Image>().fillAmount = BulletManager.GetComponent<StainManager>().manaMana / BulletManager.GetComponent<StainManager>().manaMax;
+    }
+
+    private void Update()
+    {
+        GetComponent<Image>().fillAmount = BulletManager.GetComponent<StainManager>().manaMana / BulletManager.GetComponent<StainManager>().manaMax;
+    }
+}
