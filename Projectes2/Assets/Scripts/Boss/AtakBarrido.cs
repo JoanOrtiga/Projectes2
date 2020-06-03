8c6b7418756fed9250be583936a3a6a2fd48f8bf
Marc <marcgaldo.97@gmail.com>
2020-06-03 11:59:46 +0200

    ..

--- Projectes2/Assets/Scripts/Boss/AtakBarrido.cs
+++ Projectes2/Assets/Scripts/Boss/AtakBarrido.cs
@@ -5,44 +5,120 @@ using UnityEngine;
 public class AtakBarrido : BossController
 {
     public float atackSpeed;
+    public float moveSpeed;
     public GameObject p1, p2, p3, p4;
     public int pathChoser;
-    private bool inpath;
+    private bool inpath = false;
+    private bool onMyWay=true;
+    public int ataquesBarridos;
+    private bool canStart;
+    private bool izq;
     void Start()
     {
         Random.Range(0, 1);
     }
 
-    private void Update()
+    private void OnEnable()
     {
-        if (!inpath)
+        if (this.GetComponent<SpriteRenderer>().flipX == true)
         {
-            pathChoser = Random.Range(0, 1);
+            izq = true;
         }
         else
         {
-
+            izq = false;
         }
-        
+        canStart = false;
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-        //falta implementar: saber hacia que lado mira para que vaya hacia ese lado con otro if en plan:
-        //
-        //
-        //if(pathChoser==1 && facedirection==1) {vas a la derecha }
-        //
-        //pathchoser es lo que escoge el recorrido entre el de arriba y el de abajo y facedirection escogeria
-        //hacia donde mira para que cuando salga del follow player o de donde sea vaya hacia el mismo sentido.
-        //
-        if (pathChoser == 1) 
-        { 
-            transform.position = Vector2.MoveTowards(transform.position, p1.transform.position, atackSpeed * Time.deltaTime);
-        }
-        else if(pathChoser == 0)
+
+        if (onMyWay&&izq==true)
         {
-            transform.position = Vector2.MoveTowards(transform.position, p3.transform.position, atackSpeed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, p2.transform.position, moveSpeed * Time.deltaTime);
+        }
+        else if (onMyWay && izq == false)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, p1.transform.position, moveSpeed * Time.deltaTime);
+        }
+        
+        if (canStart)
+        {
+            movement();
+        }
+      
+    }
+
+
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("LimitePantalla"))
+        {
+            onMyWay = false;
+            inpath = false;
+            canStart = false;
+            PathSelector();
+        }
+    }
+    void PathSelector()
+    {
+        if (!inpath)
+        {
+            pathChoser = Random.Range(0, 1);
+            inpath = true;
+            canStart = true;
+            if (pathChoser == 0 && izq == false)
+            {
+                transform.position = p4.transform.position;
+                gameObject.GetComponent<SpriteRenderer>().flipX=true;
+                izq = true;
+
+            }
+            else if (pathChoser == 1 && izq == false)
+            {
+                transform.position = p2.transform.position;
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                izq = true;
+
+            }
+            else if (pathChoser == 0 && izq == true)
+            {
+                transform.position = p3.transform.position;
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                izq = false;
+
+            }
+            else if (pathChoser == 1 && izq == true)
+            {
+                transform.position = p1.transform.position;
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                izq = false;
+
+            }
+        }
+
+    }
+void movement()
+    {
+        if (izq == true && pathChoser == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, p3.transform.position, moveSpeed * Time.deltaTime);
+
+        }
+        else if (izq == true && pathChoser == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, p2.transform.position, moveSpeed * Time.deltaTime);
+
+        }
+        if (izq == false && pathChoser == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, p4.transform.position, moveSpeed * Time.deltaTime);
+
+        }
+        if (izq == false && pathChoser == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, p1.transform.position, moveSpeed * Time.deltaTime);
 
         }
     }
