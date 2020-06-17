72c5f3f6b69957a72d4677ac8bfc87b871e6656b
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2020-06-17 16:06:07 +0200

    Mouse

---
+++ Projectes2/Assets/Scripts/UI/MousePointer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class MousePointer : MonoBehaviour
+{
+    public GameObject shotingMouse;
+    public GameObject normalMouse;
+
+    public bool ShootingMouseBool = true;
+
+    private GameObject pointer;
+
+
+    private void Start()
+    {
+        pointer = normalMouse;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (ShootingMouseBool == true)
+        {
+            pointer = shotingMouse;
+            shotingMouse.SetActive(true);
+            normalMouse.SetActive(false);
+
+
+        }
+        else
+        {
+            pointer = normalMouse;
+            shotingMouse.SetActive(false);
+            normalMouse.SetActive(true);
+
+
+        }
+
+
+        pointer.transform.position = Input.mousePosition;
+    }
+}
