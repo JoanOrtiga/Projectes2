9c1010c4b00d9fac43257396616df669e7ba441c
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2020-06-18 16:16:49 +0200

    Game OVer

---
+++ Projectes2/Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject mouse;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+       // this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<PlayerHealth>().currentHP >= 0)
+        {
+            mouse.GetComponent<MousePointer>().ShootingMouseBool = false;
+            this.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void restartGame()
+    {
+
+    }
+}
