a4eb7f479a81ef8c77819af2f637e3e63eaee926
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-06-19 15:37:37 +0200

    g

--- Projectes2/Assets/Scripts/CheckPoint/CheckPointManager.cs
+++ Projectes2/Assets/Scripts/CheckPoint/CheckPointManager.cs
@@ -11,15 +11,43 @@ public class CheckPointManager : MonoBehaviour
     public UnityEvent RespawnEnemies;
     Vector2 StartingPosition;
 
+    public GameObject enemiesItem;
+
+    private List<GameObject> enemiesCollection;
+    private List<Vector2> enemiesCollectionPosition;
+
+    private void Start()
+    {
+        for (int i = 0; i < enemiesItem.transform.childCount; i++)
+        {
+            enemiesCollection.Add(enemiesItem.transform.GetChild(i).gameObject);
+            enemiesCollectionPosition.Add(enemiesItem.transform.GetChild(i).position);
+        }
+    }
 
     public void Restart()
     {
-        print(StartingPosition);
         player.transform.position = StartingPosition;
         player.GetComponent<PlayerHealth>().currentHP  = player.GetComponent<PlayerHealth>().maxHP;
         bulletManager.GetComponent<StainManager>().manaMana = bulletManager.GetComponent<StainManager>().manaMax;
 
-        RespawnEnemies.Invoke();
+        for (int i = 0; i < enemiesCollection.Count; i++)
+        {
+            enemiesCollection[i].transform.position = enemiesCollectionPosition[i];
+            if (enemiesCollection[i].name == "meleEnemie")
+            {
+                enemiesCollection[i].transform.GetChild(0).GetComponent<MeleEnemie>().HP = 15;
+            }
+            else if(enemiesCollection[i].name == "shootingAlien")
+            {
+                enemiesCollection[i].GetComponent<StandardEnemie>().HP = 20;
+            }
+            else if(enemiesCollection[i].name == "FlyingEnemie")
+            {
+                enemiesCollection[i].transform.GetChild(0).GetComponent<FlyingEnemie>().HP = 15;
+            }
+        }
+        
     }
 
     public void GetCheckPoint(Vector2 pos)
