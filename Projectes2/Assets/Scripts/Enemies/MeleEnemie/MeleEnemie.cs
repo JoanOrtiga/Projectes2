125e21372486285b7f690fa59e968ed3a856cc84
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2020-06-19 15:58:07 +0200

    Enemie Mele Fix

--- Projectes2/Assets/Scripts/Enemies/MeleEnemie/MeleEnemie.cs
+++ Projectes2/Assets/Scripts/Enemies/MeleEnemie/MeleEnemie.cs
@@ -18,6 +18,7 @@ public class MeleEnemie : EnemieManager
     private bool patrolPoint;
     private bool isPatroling = true;
     private Animator animator;
+    private bool dead = false;
 
     Vector3 lastPos;
     Vector3 currentPos;
@@ -42,11 +43,18 @@ public class MeleEnemie : EnemieManager
 
         if (HP <= 0)
         {
+            foreach (BoxCollider2D item in GetComponents<BoxCollider2D>())
+            {
+                item.enabled = false;
+            }
+            dead = true;
             GameObject.FindGameObjectWithTag("BulletManager").GetComponent<StainManager>().manaCalculator(true, manaRecover);
             animator.SetBool("Dead", true);
             Destroy(this.gameObject, 2.5f);
 
             FindObjectOfType<AudioManager>().Play("EnemyDie");
+            
+
             
         }
 
@@ -56,20 +64,25 @@ public class MeleEnemie : EnemieManager
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (dead == false)
         {
-            Chase();
-            Attack();
-        }
+            if (collision.CompareTag("Player"))
+            {
+                Chase();
+                Attack();
+            }
 
-        if (player.transform.position.x < this.transform.position.x)
-        {
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+            if (player.transform.position.x < this.transform.position.x)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
         }
-        else
-        {
-            transform.localRotation = Quaternion.Euler(0, 180, 0);
-        }
+        
 
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -80,76 +93,92 @@ public class MeleEnemie : EnemieManager
 
     void Chase()
     {
-        isPatroling = false;
-        checkDirectionBool = false;
-        //  print((distance < chaseDistance && distance > attackDistance) + " Chase " + chaseDistance + " attack " + attackDistance + "distace" + distance);
-        if (distance < chaseDistance && distance > attackDistance)
+        if (dead == false)
         {
-            print("CHASE");
-            animator.SetBool("Chasing", true);
-            animator.SetBool("Attacking", false);
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, transform.position.y, transform.position.z), ChaseSpeed * Time.deltaTime);
+            isPatroling = false;
+            checkDirectionBool = false;
+            //  print((distance < chaseDistance && distance > attackDistance) + " Chase " + chaseDistance + " attack " + attackDistance + "distace" + distance);
+            if (distance < chaseDistance && distance > attackDistance)
+            {
+                print("CHASE");
+                animator.SetBool("Chasing", true);
+                animator.SetBool("Attacking", false);
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, transform.position.y, transform.position.z), ChaseSpeed * Time.deltaTime);
+            }
         }
+       
     }
 
 
     void Attack()
     {
-        isPatroling = false;
-        checkDirectionBool = false;
-        if (distance < attackDistance)
+        if (dead == false)
         {
-            animator.SetBool("Attacking", true);
-            FindObjectOfType<AudioManager>().Play("EnemyMeleAtack");
-            print("ATTACK");
+            isPatroling = false;
+            checkDirectionBool = false;
+            if (distance < attackDistance)
+            {
+                animator.SetBool("Attacking", true);
+                FindObjectOfType<AudioManager>().Play("EnemyMeleAtack");
+                print("ATTACK");
+            }
         }
+        
 
     }
 
     void patrol()
     {
-        if (isPatroling)
+        if (dead == false)
         {
-            animator.SetBool("Chasing", false);
-            animator.SetBool("Attacking", false);
-            transform.position = Vector2.MoveTowards(transform.position, spot, patrolSpeed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, spot) < 0.2f)
+            if (isPatroling)
             {
-                patrolPoint = !patrolPoint;
+                animator.SetBool("Chasing", false);
+                animator.SetBool("Attacking", false);
+                transform.position = Vector2.MoveTowards(transform.position, spot, patrolSpeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, spot) < 0.2f)
+                {
+                    patrolPoint = !patrolPoint;
 
-            }
+                }
 
-            if (patrolPoint)
-            {
-                spot = patrolPoints[1].position;
-                
+                if (patrolPoint)
+                {
+                    spot = patrolPoints[1].position;
 
-            }
-            else if (patrolPoint == false)
-            {
-                spot = patrolPoints[0].position;
-               
 
+                }
+                else if (patrolPoint == false)
+                {
+                    spot = patrolPoints[0].position;
+
+
+                }
             }
         }
+        
     }
 
     void checkDirection()
     {
-        if (checkDirectionBool)
+        if (dead == false)
         {
-            currentPos = this.transform.position;
-            //print("Last Pos:" + lastPos.x + "Current Pos" + currentPos.x);
-            if (currentPos.x < lastPos.x)
+            if (checkDirectionBool)
             {
-                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                currentPos = this.transform.position;
+                //print("Last Pos:" + lastPos.x + "Current Pos" + currentPos.x);
+                if (currentPos.x < lastPos.x)
+                {
+                    transform.localRotation = Quaternion.Euler(0, 0, 0);
+                }
+                else
+                {
+                    transform.localRotation = Quaternion.Euler(0, 180, 0);
+                }
+                lastPos = currentPos;
             }
-            else
-            {
-                transform.localRotation = Quaternion.Euler(0, 180, 0);
-            }
-            lastPos = currentPos;
         }
+        
         
     }
 }
