000b40c9abc983e8506f3e10f68b2c6fa6817711
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-06-19 11:44:56 +0200

    da

---
+++ Projectes2/Assets/ShowToolTip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class ShowToolTip : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject tooltip;
+    // When highlighted with mouse.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltip.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.SetActive(false);
+    }
+}
